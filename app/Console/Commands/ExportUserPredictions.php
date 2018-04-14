b13035a0a2862b18bdecf2f692fabb166f4253b0
Joe Fox <joefoxdevelopment@gmail.com>
2018-04-14 12:04:16 +0100

    Add script to export all saved predictions as a CSV

---
+++ app/Console/Commands/ExportUserPredictions.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\MatchData;
+use Illuminate\Console\Command;
+
+class ExportUserPredictions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'export:user-predictions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Export all user pred';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $titles = [
+            'HomeTeam',
+            'AwayTeam',
+            'HomeGoals',
+            'AwayGoals'
+        ];
+
+        $entries = MatchData::all(
+            'hometeam',
+            'awayteam',
+            'homegoals',
+            'awaygoals'
+        );
+
+        $fileHandle = fopen('exportedPredictions.csv', 'w');
+
+        fputcsv($fileHandle, $titles);
+
+        foreach ($entries as $entry) {
+            fputcsv($fileHandle, $entry->toArray());
+        }
+        fclose($fileHandle);
+
+    }
+}
